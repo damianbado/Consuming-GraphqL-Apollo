1308e29bebe9babe6e98914db39666e030771d71
damianbado <74607503+damianbado@users.noreply.github.com>
2022-03-17 18:22:47 +0100

    fragments usage

--- app/src/pages/conference/Speakers.jsx
+++ app/src/pages/conference/Speakers.jsx
@@ -1,71 +1,125 @@
 import * as React from "react";
 import "./style-sessions.css";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
 /* ---> Define queries, mutations and fragments here */
 
+// fragment definition
+const SPEAKER_ATTRIBUTES = gql`
+  fragment SpeakerInfo on Speaker {
+    id
+    name
+    bio
+    sessions {
+      id
+      title
+    }
+  }
+`;
+
+const SPEAKERS = gql`
+  query speakers {
+    speakers {
+      ...SpeakerInfo
+    }
+  }
+  ${SPEAKER_ATTRIBUTES}
+`;
+
+const SPEAKER_BY_ID = gql`
+  query speakerById($id: ID!) {
+    speakerById(id: $id) {
+      ...SpeakerInfo
+    }
+  }
+  ${SPEAKER_ATTRIBUTES}
+`;
+
 const SpeakerList = () => {
+  const { loading, error, data } = useQuery(SPEAKERS);
 
   /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
   const featured = false;
 
-  return (
-		<div
-      key={'id'}
+  if (loading) return <p>Loading speakers...</p>;
+  if (error) return <p>Error loading speakers!</p>;
+
+  return data.speakers.map(({ id, name, bio, sessions }) => (
+    <div
+      key={id}
       className="col-xs-12 col-sm-6 col-md-6"
       style={{ padding: 5 }}
     >
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'Speaker: '}</h3>
+          <h3 className="panel-title">{"Speaker: " + name}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'Bio: ' }</h5>
+          <h5>{"Bio: " + bio}</h5>
         </div>
         <div className="panel-footer">
-          <h4>Sessions</h4>
-					{
-						/* ---> Loop through speaker's sessions here */
-					}
-          <span>	
-            <button	
-              type="button"	
-              className="btn btn-default btn-lg"	
-              onClick={()=> {
+          <h3 className="panel-title"> {"Sessions"} </h3>
+          {sessions.map((session) => (
+            <span key={session.id} style={{ padding: 2 }}>
+              <p>{session.title}</p>
+            </span>
+          ))}
+
+          {/* ---> Loop through speaker's sessions here */}
+          <span>
+            <button
+              type="button"
+              className="btn btn-default btn-lg"
+              onClick={() => {
                 /* ---> Call useMutation's mutate function to mark speaker as featured */
-              }}	
-              >	
-                <i	
-                  className={`fa ${featured ? "fa-star" : "fa-star-o"}`}	
-                  aria-hidden="true"	
-                  style={{	
-                    color: featured ? "gold" : undefined,	
-                  }}	
-                ></i>{" "}	
-                Featured Speaker	
-            </button>	
+              }}
+            >
+              <i
+                className={`fa ${featured ? "fa-star" : "fa-star-o"}`}
+                aria-hidden="true"
+                style={{
+                  color: featured ? "gold" : undefined,
+                }}
+              ></i>{" "}
+              Featured Speaker
+            </button>
           </span>
         </div>
       </div>
     </div>
-	);
+  ));
 };
 
 const SpeakerDetails = () => {
+  /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  const { speaker_id } = useParams();
 
-    /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  const { loading, error, data } = useQuery(SPEAKER_BY_ID, {
+    variables: { id: speaker_id },
+  });
+
+  if (loading) return <p>Loading speakers...</p>;
+  if (error) return <p>Error loading speakers!</p>;
+
+  const speaker = data.speakerById;
+  const { id, name, bio, sessions } = speaker;
+
   return (
-    <div key={'id'} className="col-xs-12" style={{ padding: 5 }}>
+    <div key={id} className="col-xs-12" style={{ padding: 5 }}>
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'name'}</h3>
+          <h3 className="panel-title">{name}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'bio'}</h5>
+          <h5>{bio}</h5>
         </div>
         <div className="panel-footer">
-          {{
-						/* ---> Loop through speaker's sessions here */
-					}}
+          {sessions.map((session) => (
+            <span key={session.id} style={{ padding: 2 }}>
+              <p>{session.title}</p>
+            </span>
+          ))}
         </div>
       </div>
     </div>
@@ -84,7 +138,6 @@ export function Speaker() {
   );
 }
 
-
 export function Speakers() {
   return (
     <>
@@ -96,5 +149,3 @@ export function Speakers() {
     </>
   );
 }
-
-	
